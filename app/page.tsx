b5723d46c5d7e057b327a5a1b02411a550dc372e
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-29 19:04:40 -0300

    feat: post container

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import PostContainer from '@/components/PostContainer'
 import NewPost from '@/components/forms/NewPost'
+import { prisma } from '@/db/prisma'
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany()
+
   return (
     <div>
       <NewPost />
+      {posts && posts.map((post) => <PostContainer key={post.id} />)}
     </div>
   )
 }

b77ead6fe045a396dac93ae402c54fa4215709c2
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-29 18:48:26 -0300

    feat: header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import Header from '@/components/Header'
 import { cn } from '@/lib/ultis'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ weight: '400', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Buildbox Challenge',
@@ -17,8 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className="!scroll-smooth">
-      <body className={cn(inter.className, 'font-sans antialiased')}>
+      <body className={cn(roboto.className, 'font-sans antialiased')}>
         <main className="relative flex flex-col min-h-dvh overflow-hidden">
+          <Header />
           {children}
         </main>
       </body>
---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="navbar justify-center bg-secondary min-h-24">
+      <div className="flex flex-col ">
+        <h1 className="text-2xl font-bold text-primary">buildbox</h1>
+        <p className="text-sm text-secondary-content">WEB CHALLENCE</p>
+      </div>
+    </header>
+  )
+}

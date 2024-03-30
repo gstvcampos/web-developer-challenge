7c455bf83cc23ec3c34f4314d96673442f3ade08
gstvcampos <gustavo.bmc76@gmail.com>
2024-03-30 13:05:11 -0300

    feat: container

--- components/layout/PostContainer.tsx
+++ components/layout/PostContainer.tsx
@@ -2,9 +2,17 @@ import { Post } from '@prisma/client'
 
 export default function PostContainer({ post }: { post: Post }) {
   return (
-    <div>
-      <h3>{post.author}</h3>
-      <p>{post.content}</p>
+    <div className="flex flex-cpl sm:flex-row ">
+      <div>
+        <h2>{post.author}</h2>
+      </div>
+      <div>
+        <p>{post.content}</p>
+        <div>
+          <span>Enviado por</span>
+          <span>{post.author}</span>
+        </div>
+      </div>
     </div>
   )
 }
